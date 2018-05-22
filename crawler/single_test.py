4d5b33ab7ffda6fd1f5ebf4ac7a9c75b3a59b63f
ikimonogakari <zhaosukima@gmail.com>
2018-05-22 00:49:35 -0700

    get single page successfully

--- crawler/single_test.py
+++ crawler/single_test.py
@@ -1,1 +1,24 @@
-import request
+import requests
+
+target_url = 'https://egov.uscis.gov/casestatus/mycasestatus.do'
+userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+
+def craw_page(formdata, http_session):
+    http_session.headers['User-Agent'] = userAgent
+    r = http_session.post(target_url, data=formdata)
+    return r.text
+
+def get_single_page():
+    s = requests.Session()
+    http_form = {
+        'appReceiptNum': 'YSC1890125000',
+        'initCaseSearch': 'CHECK+STATUS',
+        'changeLocale': ''
+        }
+    raw_html = craw_page(http_form, s)
+    f = open('result.html', 'w')
+    f.write(raw_html)
+
+
+if __name__ == '__main__':
+    get_single_page()
